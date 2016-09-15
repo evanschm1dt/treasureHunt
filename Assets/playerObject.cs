914a09223d028f45157ee9fae1cb5b025c1065af
Evan Schmidt <ejs552@nyu.edu>
2016-09-15 16:39:51 -0400

    added beginning stuff

---
+++ Assets/playerObject.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.W)) {
+			transform.position += new Vector3 (0, 0, 20) * Time.deltaTime;
+			transform.eulerAngles = new Vector3 (0, 90, 0);
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.position += new Vector3 (0, 0, -20) * Time.deltaTime;
+			transform.eulerAngles = new Vector3 (0, -90, 0);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.position += new Vector3 (20, 0, 0) * Time.deltaTime;
+			transform.eulerAngles = new Vector3 (0, 180, 0);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.position += new Vector3 (-20, 0, 0) * Time.deltaTime;
+			transform.eulerAngles = new Vector3 (0, -180, 0);
+		}
+		Camera.main.transform.position = transform.position + new Vector3 (0, 8, 0);
+	}
+}
